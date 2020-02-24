14f063372b443b7f60de2902a31a4b089a18d9e7
oserna1 <oserna1994@gmail.com>
2020-02-23 23:46:55 -0600

    fixed es lint issue

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>Events Listing</h1>
 
-    <router-link :to="{ name: 'event-show', params: { id: '1'} }">Show Event #1</router-link>
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >Show Event #1</router-link
+    >
   </div>
 </template>
